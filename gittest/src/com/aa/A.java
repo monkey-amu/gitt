51e2de9ff12ad430b6150f7d11408ce9cc247892
root <8647446632@qq.com>
2018-11-25 13:54:47 +0800

    aaa

---
+++ gittest/src/com/aa/A.java
@@ -0,0 +1,11 @@
+package com.aa;
+
+public class A {
+    public static void main(String[] args) {
+        System.out.println("gitniubi");
+        System.out.println("gitniubi");
+        System.out.println("gitniubi");
+        System.out.println("gitniubi");
+        System.out.println("gitniubi");
+    }
+}
