fba33300b92b3018cac484a8b6b79f4614475f97
root <8647446632@qq.com>
2018-11-25 14:06:13 +0800

    xiug

--- gittest/src/com/aa/A.java
+++ gittest/src/com/aa/A.java
@@ -14,5 +14,8 @@ public class A {
         System.out.println("ggsimida");
         System.out.println("ggsimida");
         System.out.println("ggsimida");
+        for (int i = 0; i < 100; i++) {
+            System.out.println(i);
+        }
     }
 }
